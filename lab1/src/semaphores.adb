299eca24cd41f4ba399c380b26ba2c8d35845c7b
tjyu1999 <87049229+tjyu1999@users.noreply.github.com>
2023-09-14 00:31:52 +0200

    Add files via upload

--- lab1/src/semaphores.adb
+++ lab1/src/semaphores.adb
@@ -1,15 +1,19 @@
 -- Package: Semaphores
+--
+-- ==> Complete the code at the indicated places
 
 package body Semaphores is
    protected body CountingSemaphore is
+      -- To be completed
       entry Wait When Count > 0 is
          begin
-         Count := Count - 1;
-      end Wait;
-       
-      entry Signal when Count < MaxCount is
+         Count := Count - 1; -- decrement
+      end wait;
+      
+      -- To be completed
+      entry Signal When Count < MaxCount is
          begin
-         Count := Count + 1;
+         Count := Count + 1; --increment
       end Signal;
    end CountingSemaphore;
 end Semaphores;

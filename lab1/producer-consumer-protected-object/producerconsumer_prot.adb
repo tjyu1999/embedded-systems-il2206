d77d1039e2f2682270cccc37a750acd0a22c965d
tjyu1999 <87049229+tjyu1999@users.noreply.github.com>
2023-09-25 00:44:52 +0200

    Add files via upload

---
+++ lab1/producer-consumer-protected-object/producerconsumer_prot.adb
@@ -0,0 +1,67 @@
+with Ada.Text_IO;
+use Ada.Text_IO;
+
+with Ada.Real_Time;
+use Ada.Real_Time;
+
+with Buffer;
+use Buffer;
+
+with Ada.Numerics.Discrete_Random;
+
+procedure ProducerConsumer_Prot is
+
+   N : constant Integer := 10; -- Number of produced and consumed tokens per task
+   X : constant Integer := 3; -- Number of producers and consumers
+   
+   B : CircularBuffer;
+	
+   -- Random Delays
+   subtype Delay_Interval is Integer range 50..250;
+   package Random_Delay is new Ada.Numerics.Discrete_Random (Delay_Interval);
+   use Random_Delay;
+   G : Generator;
+
+   -- ==> Complete code: Use Buffer
+
+   task type Producer;
+
+   task type Consumer;
+
+   task body Producer is
+      Next : Time;
+   begin
+      Next := Clock;
+      for I in 1..N loop
+         -- ==> Complete code: Write to Buffer
+         B.Put(I);
+
+         -- Next 'Release' in 50..250ms
+         Next := Next + Milliseconds(Random(G));
+         delay until Next;
+      end loop;
+   end;
+
+   task body Consumer is
+      Next : Time;
+      X : Integer;
+   begin
+      Next := Clock;
+      for I in 1..N loop
+         -- Read from X	
+         -- ==> Complete code: Read from Buffer
+         B.Get(X);
+         Put_Line(Integer'Image(X));
+
+         -- Next 'Release' in 50..250mss
+         Next := Next + Milliseconds(Random(G));
+         delay until Next;
+      end loop;
+   end;
+	
+   P: array (Integer range 1..X) of Producer;
+   C: array (Integer range 1..X) of Consumer;
+
+begin -- main task
+   null;
+end ProducerConsumer_Prot;

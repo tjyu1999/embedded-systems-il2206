898d0aa0486069fb4ee2fb889c8904e0593f1605
tjyu1999 <87049229+tjyu1999@users.noreply.github.com>
2023-10-04 21:41:24 +0200

    Create Handshake.c

---
+++ lab2/Handshake/Handshake.c
@@ -0,0 +1,133 @@
+// File: Handshake.c 
+
+# include <stdio.h>
+# include "includes.h"
+# include <string.h>
+
+# define DEBUG 1
+
+/* Definition of Task Stacks */
+/* Stack grows from HIGH to LOW memory */
+# define   TASK_STACKSIZE       2048
+OS_STK    task1_stk[TASK_STACKSIZE];
+OS_STK    task2_stk[TASK_STACKSIZE];
+OS_STK    stat_stk[TASK_STACKSIZE];
+
+OS_EVENT *semaphore1;
+OS_EVENT *semaphore2;
+
+/* Definition of Task Priorities */
+# define TASK1_PRIORITY      6  // highest priority
+# define TASK2_PRIORITY      7
+# define TASK_STAT_PRIORITY 12  // lowest priority
+
+int task1_state = 1;
+int task2_state = 0;
+
+void print_task_state(INT8U task_id, INT8U task_state){
+    printf("Task %d - State %d\n", task_id - 1, task_state);
+}
+
+void printStackSize(char* name, INT8U prio){
+    INT8U err;
+    OS_STK_DATA stk_data;
+    
+    err = OSTaskStkChk(prio, &stk_data);
+    if(err == OS_NO_ERR){
+        if(DEBUG == 1)
+            printf("%s (priority %d) - Used: %d; Free: %d\n", name, prio, (int) stk_data.OSUsed, (int) stk_data.OSFree);
+    }
+    else{
+        if(DEBUG == 1)
+	    printf("Stack Check Error!\n");    
+    }
+}
+
+/* Prints a message and sleeps for given time interval */
+void task1(void* pdata){
+    INT8U err;
+
+    while(1){        
+        OSSemPend(semaphore1, 0, &err);
+        task1_state = 1 - task1_state;
+        task2_state = task1_state;
+        print_task_state(1, task1_state);
+        print_task_state(2, task2_state);
+        OSSemPost(semaphore2);
+    }
+}
+
+/* Prints a message and sleeps for given time interval */
+void task2(void* pdata){
+    INT8U err;
+
+    while(1){
+        OSSemPend(semaphore2, 0, &err);
+        task2_state = 1 - task2_state;
+        task1_state = task2_state;
+        print_task_state(2, task2_state);
+        print_task_state(1, task1_state);
+        OSSemPost(semaphore1);
+    }
+}
+
+/* Printing Statistics */
+void statisticTask(void* pdata){
+    while(1){
+        printStackSize("Task1", TASK1_PRIORITY);
+        printStackSize("Task2", TASK2_PRIORITY);
+        printStackSize("StatisticTask", TASK_STAT_PRIORITY);
+    }
+}
+
+/* The main function creates two task and starts multi-tasking */
+int main(void){
+    printf("Lab 3 - Two Tasks\n");
+    
+    semaphore1 = OSSemCreate(1);
+    semaphore2 = OSSemCreate(0);
+
+    OSTaskCreateExt(
+        task1,                        // Pointer to task code
+        NULL,                         // Pointer to argument passed to task
+        &task1_stk[TASK_STACKSIZE-1], // Pointer to top of task stack
+        TASK1_PRIORITY,               // Desired Task priority
+        TASK1_PRIORITY,               // Task ID
+        &task1_stk[0],                // Pointer to bottom of task stack
+        TASK_STACKSIZE,               // Stacksize
+        NULL,                         // Pointer to user supplied memory (not needed)
+        OS_TASK_OPT_STK_CHK |         // Stack Checking enabled 
+        OS_TASK_OPT_STK_CLR           // Stack Cleared                                 
+        );
+	   
+    OSTaskCreateExt(
+        task2,                        // Pointer to task code
+        NULL,                         // Pointer to argument passed to task
+        &task2_stk[TASK_STACKSIZE-1], // Pointer to top of task stack
+        TASK2_PRIORITY,               // Desired Task priority
+        TASK2_PRIORITY,               // Task ID
+        &task2_stk[0],                // Pointer to bottom of task stack
+        TASK_STACKSIZE,               // Stacksize
+        NULL,                         // Pointer to user supplied memory (not needed)
+        OS_TASK_OPT_STK_CHK |         // Stack Checking enabled 
+        OS_TASK_OPT_STK_CLR           // Stack Cleared                       
+        );  
+
+    if(DEBUG == 1){
+        OSTaskCreateExt(
+            statisticTask,                // Pointer to task code
+	        NULL,                         // Pointer to argument passed to task
+	        &stat_stk[TASK_STACKSIZE-1],  // Pointer to top of task stack
+	        TASK_STAT_PRIORITY,           // Desired Task priority
+	        TASK_STAT_PRIORITY,           // Task ID
+	        &stat_stk[0],                 // Pointer to bottom of task stack
+	        TASK_STACKSIZE,               // Stacksize
+	        NULL,                         // Pointer to user supplied memory (not needed)
+	        OS_TASK_OPT_STK_CHK |         // Stack Checking enabled 
+	        OS_TASK_OPT_STK_CLR           // Stack Cleared                              
+	    );
+    }  
+
+    OSStart();
+    return 0;
+}

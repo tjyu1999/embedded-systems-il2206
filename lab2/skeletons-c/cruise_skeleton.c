e8697a7e96a2bfbe9c6a49a0aded2c69247a30bb
tjyu1999 <87049229+tjyu1999@users.noreply.github.com>
2024-02-25 11:05:57 +0800

    Update cruise_skeleton.c

--- lab2/skeletons-c/cruise_skeleton.c
+++ lab2/skeletons-c/cruise_skeleton.c
@@ -51,9 +51,7 @@
 /*
  * Definition of Tasks
  */
-
 # define TASK_STACKSIZE 2048
-
 OS_STK StartTask_Stack[TASK_STACKSIZE]; 
 OS_STK ControlTask_Stack[TASK_STACKSIZE]; 
 OS_STK VehicleTask_Stack[TASK_STACKSIZE];
@@ -84,7 +82,7 @@ OS_EVENT* Mbox_Engine;
 /*
  * Types
  */
-enum active {on = 2, off = 1};
+enum active{on = 2, off = 1};
 
 /*
  * Global variables
@@ -97,7 +95,7 @@ INT32U led_red = 0;   // Red LEDs
  * Helper functions
  */
 
-int buttons_pressed(void) return ~IORD_ALTERA_AVALON_PIO_DATA(D2_PIO_KEYS4_BASE);    
+int buttons_pressed(void) return ~IORD_ALTERA_AVALON_PIO_DATA(D2_PIO_KEYS4_BASE);
 
 int switches_pressed(void) return IORD_ALTERA_AVALON_PIO_DATA(DE2_PIO_TOGGLES18_BASE);
 
@@ -109,18 +107,17 @@ alt_u32 alarm_handler(void* context){
     return delay;
 }
 
-static int b2sLUT[] = {0x40, //0
-                       0x79, //1
-                       0x24, //2
-                       0x30, //3
-                       0x19, //4
-                       0x12, //5
-                       0x02, //6
-                       0x78, //7
-                       0x00, //8
-                       0x18, //9
-                       0x3F, //-
-};
+static int b2sLUT[] = {0x40,  //0
+                       0x79,  //1
+                       0x24,  //2
+                       0x30,  //3
+                       0x19,  //4
+                       0x12,  //5
+                       0x02,  //6
+                       0x78,  //7
+                       0x00,  //8
+                       0x18,  //9
+                       0x3F}; //-
 
 /*
  * convert int to seven segment display format
@@ -144,13 +141,13 @@ void show_velocity_on_sevenseg(INT8S velocity){
     else out_sign = int2seven(0);
 
     out_high = int2seven(tmp / 10);
-    out_low = int2seven(tmp - (tmp/10) * 10);
-
+    out_low = int2seven(tmp - (tmp / 10) * 10);
     out = int2seven(0) << 21 |
           out_sign << 14 |
           out_high << 7  |
           out_low;
-    IOWR_ALTERA_AVALON_PIO_DATA(DE2_PIO_HEX_LOW28_BASE,out);
+    
+    IOWR_ALTERA_AVALON_PIO_DATA(DE2_PIO_HEX_LOW28_BASE, out);
 }
 
 /*
@@ -185,7 +182,7 @@ void VehicleTask(void* pdata){
     const unsigned int wind_factor = 1;
     const unsigned int brake_factor = 4;
     const unsigned int gravity_factor = 2;
-    
+
     // variables relevant to the model and its simulation on top of the RTOS
     INT8U err;  
     void* msg;
@@ -199,192 +196,159 @@ void VehicleTask(void* pdata){
     printf("Vehicle task created!\n");
 
     while(1){
-        err = OSMboxPost(Mbox_Velocity, (void*) &velocity);
+        err = OSMboxPost(Mbox_Velocity, (void *) &velocity);
 
-        OSTimeDlyHMSM(0,0,0,VEHICLE_PERIOD);
+        OSTimeDlyHMSM(0, 0, 0, VEHICLE_PERIOD); 
 
         /* Non-blocking read of mailbox: 
            - message in mailbox: update throttle
            - no message:         use old throttle
-        */
+         */
         msg = OSMboxPend(Mbox_Throttle, 1, &err); 
-        if (err == OS_NO_ERR) throttle = (INT8U*) msg;
-    /* Same for the brake signal that bypass the control law */
-    msg = OSMboxPend(Mbox_Brake, 1, &err); 
-    if (err == OS_NO_ERR) 
-      brake_pedal = (enum active) msg;
-    /* Same for the engine signal that bypass the control law */
-    msg = OSMboxPend(Mbox_Engine, 1, &err); 
-    if (err == OS_NO_ERR) 
-      engine = (enum active) msg;
+        if(err == OS_NO_ERR) throttle = (INT8U*) msg;
+        /* Same for the brake signal that bypass the control law */
+        msg = OSMboxPend(Mbox_Brake, 1, &err); 
+        if(err == OS_NO_ERR) brake_pedal = (enum active) msg;
+        /* Same for the engine signal that bypass the control law */
+        msg = OSMboxPend(Mbox_Engine, 1, &err); 
+        if(err == OS_NO_ERR) engine = (enum active) msg;
 
+        // vehichle cannot effort more than 80 units of throttle
+        if(*throttle > 80) *throttle = 80;
+        
+        // brakes + wind
+        if(brake_pedal == off){
+            // wind resistance
+            acceleration = -wind_factor * velocity;
+            // actuate with engines
+            if(engine == on) acceleration += (*throttle);
 
-    // vehichle cannot effort more than 80 units of throttle
-    if (*throttle > 80) *throttle = 80;
+            // gravity effects
+            if(400 <= position && position < 800) acceleration -= gravity_factor;            // traveling uphill
+            else if(800 <= position && position < 1200) acceleration -= 2 * gravity_factor;  // traveling steep uphill
+            else if(1600 <= position && position < 2000) acceleration += 2 * gravity_factor; //traveling downhill
+            else if(2000 <= position) acceleration += gravity_factor;                        // traveling steep downhill
+        }
+    
+        // if the engine and the brakes are activated at the same time,
+        // we assume that the brake dynamics dominates, so both cases fall
+        // here.
+        else acceleration = -brake_factor * velocity;
 
-    // brakes + wind
-    if (brake_pedal == off)
-    {
-      // wind resistance
-      acceleration = - wind_factor*velocity;
-      // actuate with engines
-      if (engine == on)
-        acceleration += (*throttle);
+        printf("Position: %d m\n", position);
+        printf("Velocity: %d m/s\n", velocity);
+        printf("Accell: %d m/s2\n", acceleration);
+        printf("Throttle: %d V\n", *throttle);
 
-      // gravity effects
-      if (400 <= position && position < 800)
-        acceleration -= gravity_factor; // traveling uphill
-      else if (800 <= position && position < 1200)
-        acceleration -= 2*gravity_factor; // traveling steep uphill
-      else if (1600 <= position && position < 2000)
-        acceleration += 2*gravity_factor; //traveling downhill
-      else if (2000 <= position)
-        acceleration += gravity_factor; // traveling steep downhill
+        position = position + velocity * VEHICLE_PERIOD / 1000;
+        velocity = velocity  + acceleration * VEHICLE_PERIOD / 1000.0;
+        // reset the position to the beginning of the track
+        if(position > 2400) position = 0;
+
+        show_velocity_on_sevenseg((INT8S) velocity);
     }
-    // if the engine and the brakes are activated at the same time,
-    // we assume that the brake dynamics dominates, so both cases fall
-    // here.
-    else 
-      acceleration = - brake_factor*velocity;
-
-    printf("Position: %d m\n", position);
-    printf("Velocity: %d m/s\n", velocity);
-    printf("Accell: %d m/s2\n", acceleration);
-    printf("Throttle: %d V\n", *throttle);
-
-    position = position + velocity * VEHICLE_PERIOD / 1000;
-    velocity = velocity  + acceleration * VEHICLE_PERIOD / 1000.0;
-    // reset the position to the beginning of the track
-    if(position > 2400)
-      position = 0;
-
-    show_velocity_on_sevenseg((INT8S) velocity);
-  }
 } 
 
 /*
  * The task 'ControlTask' is the main task of the application. It reacts
  * on sensors and generates responses.
  */
+void ControlTask(void* pdata){
+    INT8U err;
+    INT8U throttle = 40; /* Value between 0 and 80, which is interpreted as between 0.0V and 8.0V */
+    void* msg;
+    INT16S* current_velocity;
 
-void ControlTask(void* pdata)
-{
-  INT8U err;
-  INT8U throttle = 40; /* Value between 0 and 80, which is interpreted as between 0.0V and 8.0V */
-  void* msg;
-  INT16S* current_velocity;
+    enum active gas_pedal = off;
+    enum active top_gear = off;
+    enum active cruise_control = off; 
 
-  enum active gas_pedal = off;
-  enum active top_gear = off;
-  enum active cruise_control = off; 
+    printf("Control Task created!\n");
 
-  printf("Control Task created!\n");
+    while(1){
+        msg = OSMboxPend(Mbox_Velocity, 0, &err);
+        current_velocity = (INT16S*) msg;
 
-  while(1)
-  {
-    msg = OSMboxPend(Mbox_Velocity, 0, &err);
-    current_velocity = (INT16S*) msg;
+        // Here you can use whatever technique or algorithm that you prefer to control
+        // the velocity via the throttle. There are no right and wrong answer to this controller, so
+        // be free to use anything that is able to maintain the cruise working properly. You are also
+        // allowed to store more than one sample of the velocity. For instance, you could define
+        //
+        // INT16S previous_vel;
+        // INT16S pre_previous_vel;
+        // ...
+        //
+        // If your control algorithm/technique needs them in order to function. 
 
-    // Here you can use whatever technique or algorithm that you prefer to control
-    // the velocity via the throttle. There are no right and wrong answer to this controller, so
-    // be free to use anything that is able to maintain the cruise working properly. You are also
-    // allowed to store more than one sample of the velocity. For instance, you could define
-    //
-    // INT16S previous_vel;
-    // INT16S pre_previous_vel;
-    // ...
-    //
-    // If your control algorithm/technique needs them in order to function. 
-
-    err = OSMboxPost(Mbox_Throttle, (void *) &throttle);
-
-    OSTimeDlyHMSM(0,0,0, CONTROL_PERIOD);
-  }
+        err = OSMboxPost(Mbox_Throttle, (void*) &throttle);
+        OSTimeDlyHMSM(0, 0, 0, CONTROL_PERIOD);
+    }
 }
 
 /* 
  * The task 'StartTask' creates all other tasks kernel objects and
  * deletes itself afterwards.
  */ 
+void StartTask(void* pdata){
+    INT8U err;
+    void* context;
+    static alt_alarm alarm; /* Is needed for timer ISR function */
 
-void StartTask(void* pdata)
-{
-  INT8U err;
-  void* context;
+    /* Base resolution for SW timer : HW_TIMER_PERIOD ms */
+    delay = alt_ticks_per_second() * HW_TIMER_PERIOD / 1000; 
+    printf("delay in ticks %d\n", delay);
 
-  static alt_alarm alarm;     /* Is needed for timer ISR function */
+    /* 
+     * Create Hardware Timer with a period of 'delay' 
+     */
+    if(alt_alarm_start(&alarm, delay, alarm_handler, context) < 0) printf("No system clock available!n");
 
-  /* Base resolution for SW timer : HW_TIMER_PERIOD ms */
-  delay = alt_ticks_per_second() * HW_TIMER_PERIOD / 1000; 
-  printf("delay in ticks %d\n", delay);
+    /* 
+     * Create and start Software Timer 
+     */
 
-  /* 
-   * Create Hardware Timer with a period of 'delay' 
-   */
-  if (alt_alarm_start (&alarm,
-        delay,
-        alarm_handler,
-        context) < 0)
-  {
-    printf("No system clock available!n");
-  }
+    /*
+     * Creation of Kernel Objects
+     */
 
-  /* 
-   * Create and start Software Timer 
-   */
+    // Mailboxes
+    Mbox_Throttle = OSMboxCreate((void*) 0); /* Empty Mailbox - Throttle */
+    Mbox_Velocity = OSMboxCreate((void*) 0); /* Empty Mailbox - Velocity */
+    Mbox_Brake = OSMboxCreate((void*) 1);    /* Empty Mailbox - Velocity */
+    Mbox_Engine = OSMboxCreate((void*) 1);   /* Empty Mailbox - Engine */
 
-  /*
-   * Creation of Kernel Objects
-   */
+    /*
+     * Create statistics task
+     */
+    OSStatInit();
 
-  // Mailboxes
-  Mbox_Throttle = OSMboxCreate((void*) 0); /* Empty Mailbox - Throttle */
-  Mbox_Velocity = OSMboxCreate((void*) 0); /* Empty Mailbox - Velocity */
-  Mbox_Brake = OSMboxCreate((void*) 1); /* Empty Mailbox - Velocity */
-  Mbox_Engine = OSMboxCreate((void*) 1); /* Empty Mailbox - Engine */
+    /* 
+     * Creating Tasks in the system 
+     */
+    err = OSTaskCreateExt(ControlTask,                          // Pointer to task code
+                          NULL,                                 // Pointer to argument that is passed to task
+                          &ControlTask_Stack[TASK_STACKSIZE-1], // Pointer to top of task stack
+                          CONTROLTASK_PRIO,
+                          CONTROLTASK_PRIO,
+                          (void*) &ControlTask_Stack[0],
+                          TASK_STACKSIZE,
+                          (void*) 0,
+                          OS_TASK_OPT_STK_CHK);
 
-  /*
-   * Create statistics task
-   */
+    err = OSTaskCreateExt(VehicleTask,                          // Pointer to task code
+                          NULL,                                 // Pointer to argument that is passed to task
+                          &VehicleTask_Stack[TASK_STACKSIZE-1], // Pointer to top of task stack
+                          VEHICLETASK_PRIO,
+                          VEHICLETASK_PRIO,
+                          (void*) &VehicleTask_Stack[0],
+                          TASK_STACKSIZE,
+                          (void*) 0,
+                          OS_TASK_OPT_STK_CHK);
 
-  OSStatInit();
+    printf("All Tasks and Kernel Objects generated!\n");
 
-  /* 
-   * Creating Tasks in the system 
-   */
-
-
-  err = OSTaskCreateExt(
-      ControlTask, // Pointer to task code
-      NULL,        // Pointer to argument that is
-      // passed to task
-      &ControlTask_Stack[TASK_STACKSIZE-1], // Pointer to top
-      // of task stack
-      CONTROLTASK_PRIO,
-      CONTROLTASK_PRIO,
-      (void *)&ControlTask_Stack[0],
-      TASK_STACKSIZE,
-      (void *) 0,
-      OS_TASK_OPT_STK_CHK);
-
-  err = OSTaskCreateExt(
-      VehicleTask, // Pointer to task code
-      NULL,        // Pointer to argument that is
-      // passed to task
-      &VehicleTask_Stack[TASK_STACKSIZE-1], // Pointer to top
-      // of task stack
-      VEHICLETASK_PRIO,
-      VEHICLETASK_PRIO,
-      (void *)&VehicleTask_Stack[0],
-      TASK_STACKSIZE,
-      (void *) 0,
-      OS_TASK_OPT_STK_CHK);
-
-  printf("All Tasks and Kernel Objects generated!\n");
-
-  /* Task deletes itself */
-
-  OSTaskDel(OS_PRIO_SELF);
+    /* Task deletes itself */
+    OSTaskDel(OS_PRIO_SELF);
 }
 
 /*
@@ -393,25 +357,19 @@ void StartTask(void* pdata)
  * the OS. All other tasks are started from the task 'StartTask'.
  *
  */
+int main(void){
+    printf("Lab: Cruise Control\n");
 
-int main(void) {
+    OSTaskCreateExt(StartTask,                                  // Pointer to task code
+                    NULL,                                       // Pointer to argument that is passed to task
+                    (void*) &StartTask_Stack[TASK_STACKSIZE-1], // Pointer to top of task stack 
+                    STARTTASK_PRIO,
+                    STARTTASK_PRIO,
+                    (void*) &StartTask_Stack[0],
+                    TASK_STACKSIZE,
+                    (void*) 0,  
+                    OS_TASK_OPT_STK_CHK | OS_TASK_OPT_STK_CLR);
 
-  printf("Lab: Cruise Control\n");
-
-  OSTaskCreateExt(
-      StartTask, // Pointer to task code
-      NULL,      // Pointer to argument that is
-      // passed to task
-      (void *)&StartTask_Stack[TASK_STACKSIZE-1], // Pointer to top
-      // of task stack 
-      STARTTASK_PRIO,
-      STARTTASK_PRIO,
-      (void *)&StartTask_Stack[0],
-      TASK_STACKSIZE,
-      (void *) 0,  
-      OS_TASK_OPT_STK_CHK | OS_TASK_OPT_STK_CLR);
-
-  OSStart();
-
-  return 0;
+    OSStart();
+    return 0;
 }
